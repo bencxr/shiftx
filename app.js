d62743be5d9cef70b58b5c77afa6929973533047
Ben Chan <benchan@bitgo.com>
2016-06-26 21:02:52 -0700

    add everything

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var routes = require('./app/routes');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/shiftx'); // connect to our database
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// uncomment after placing your favicon in /public
+// var favicon = require('serve-favicon');
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+var port = process.env.PORT || 4000;        // set our port
+app.listen(port);
+console.log("Listening on port 4000..");
