6805f09ae58795a25768768f3552f1b67d87b6c8
Barath Chandrashekar <barath@bitgo.com>
2016-06-27 01:26:16 -0700

    Started shift funtionality

--- public/js/shiftx.js
+++ public/js/shiftx.js
@@ -3,24 +3,28 @@ var shiftX = angular.module('shiftX', ['ngRoute']);
 
 shiftX.controller('mainController', ['$rootScope', '$scope', '$http', '$location', function($rootScope, $scope, $http, $location) {
     $scope.formData = {};
-    $scope.id = "";
+    $rootScope.id = "";
     var conversion = true;
+    var pair = 'btceth';
+    $scope.address=''
 
     function getRate() {
         var url;
         if (conversion) {
             $scope.firstImage = "bitcoin";
             $scope.secondImage = "ethereum";
+            pair = 'btceth';
             url = 'http://localhost:4000/api/pair/btceth';
         } else {
             $scope.firstImage = "ethereum";
             $scope.secondImage = "bitcoin";
+            pair = 'ethbtc';
             url = 'http://localhost:4000/api/pair/ethbtc';   
         }
         $http.get(url)
         .success(function(data) {
             $scope.rate = Math.round(data.rate*10000)/10000;
-            $scope.id = data.id;
+            $rootScope.id = data.id;
         })
         .error(function(data) {
             console.log('Error: ' + data);
@@ -33,10 +37,52 @@ shiftX.controller('mainController', ['$rootScope', '$scope', '$http', '$location
     };
 
     $scope.startShift = function() {
-
+        var params = {
+            pair: pair,
+            withdrawAddress: $scope.address
+        };
+        $http.post('http://localhost:4000/api/shift', params)
+        .success(function(data) {
+            $rootScope.id = data.id;
+            console.log(data);
+            $location.path('/shift/'+ $rootScope.id);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
     };
 
     getRate();
+}]);
+
+shiftX.controller('shiftController', ['$rootScope', '$scope', '$http', '$location', function($rootScope, $scope, $http, $location) {
+
+    $scope.conversionDirection = "bitcoin";
+
+    var getIdFromUrl = function() {
+        var url = $location.path().split('/');
+        var curIdIdx = url.indexOf("shift") + 1;
+        return url[curIdIdx];
+    }
+
+    function getConversion() {
+        var id = getIdFromUrl();
+        var url = 'http://localhost:4000/api/shift/' + id;
+        $http.get(url)
+        .success(function(data) {
+            $rootScope.expires = moment(data.expires).format('MMMM Do YYYY, h:mm:ss A');
+            $rootScope.depositAddress = data.depositAddress;
+            // change conversion direction here
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+            // just redirect to home page incase of error (possibly cause of wrong id)
+            //$location.path('/');
+        });
+    }
+
+    getConversion();
 }]);
 
 
